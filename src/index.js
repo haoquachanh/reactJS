5a735f08c7a0bd51a1244e536d2c40b244b6d9ea
haoquachanh <anhhaomnmm@gmail.com>
2022-07-14 05:10:51 +0700

    structure file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 // import ReactDOM from 'react-dom/client';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './views/App';
 import reportWebVitals from './reportWebVitals';
+import './styles/global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
