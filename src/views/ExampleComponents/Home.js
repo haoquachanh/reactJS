d2b9049bcb29a568645622830cd56f0654bafdd8
haoquachanh <anhhaomnmm@gmail.com>
2022-07-23 04:52:45 +0700

    import img

--- src/views/ExampleComponents/Home.js
+++ src/views/ExampleComponents/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import logo from '../../assets/img/logo.png'
 
 
 class Home extends React.Component{
     render(){
         return(
+            <>
             <div>Welcome Home Page!</div>
+            <img src={logo} style={{height:'500px', width:'600px'}}/>
+            </>
         )
     }
 }
