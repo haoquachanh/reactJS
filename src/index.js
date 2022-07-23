d3d03061f966ac76f9b0f7a7eb0da1f5c36f1ee0
haoquachanh <anhhaomnmm@gmail.com>
2022-07-24 02:49:02 +0700

    end. (redux will be finished in another PJ)

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ import App from './views/App';
 import reportWebVitals from './reportWebVitals';
 import './styles/global.scss';
 
+import {Provider} from "react-redux"
+import {createStore} from "redux"
+import './store/reducers/rootReducer'
+import rootReducer from './store/reducers/rootReducer';
+
+const reduxStore = createStore(rootReducer)
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={reduxStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,13 @@
+const initState={
+    users:[
+        {id: 1, name: TangSen},
+        {id: 2, name: WuKong}
+    ]
+}
+
+const rootReducer=(state=initState,action)=>{
+    
+    return state
+}
+
+export default rootReducer;
